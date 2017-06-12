b0e989e1d681768d4cefa1439da79d653d1d68fc
shuangrain <shuangrain07@gmail.com>
2017-06-12 13:33:13 +0800

    優化&加入GA

--- src/Dm5.js
+++ src/Dm5.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         exFastComic - Dm5
-// @namespace    http://blog.exfast.me/
-// @version      0.1
+// @namespace    https://github.com/shuangrain/exFastComic
+// @version      v20170612
 // @description  滾滾滾看漫畫 !!
-// @author       shuangrain07@gmail.com
+// @author       shuangrain
 // @match        http://www.dm5.com/*
 // @grant        none
 // ==/UserScript==
@@ -11,66 +11,84 @@
 (function() {
     'use strict';
 
+    var versionTag = 'exFastComic_v20170612_';
+    var authorTag = 'https://github.com/shuangrain/exFastComic';
+
     if ($('#showimage').length > 0) {
         Run();
     }
 
     function Run() {
         var currentIndex = DM5_FLOAT_INDEX;
+        var imgApi = 'chapterfun.ashx?cid=';
         var isLock = false;
 
         var arrUrl = function() {
             var tmpArr = [];
             var pageDom = $($('div.pageBar')[0]).find('a');
+
             $.each(pageDom, function(index, item) {
                 var href = $(item).attr('href');
-                var api = 'chapterfun.ashx?cid=';
                 var value = function() {
                     var tmp = href.substring(0, href.indexOf('-'));
                     tmp = tmp.substring(tmp.indexOf('/m') + 2);
                     return tmp;
                 }();
-                tmpArr.push(href + api + value + '&page=' + parseInt(index + 1));
+
+                tmpArr.push(href + imgApi + value + '&page=' + parseInt(index + 1));
             });
 
             return tmpArr;
         }();
 
-        //處理第一張圖
-        (function() {
-            $('div#cp_img').html('');
-            loadImage(arrUrl[currentIndex - 1]);
-        })();
+        //重新讀取第一張圖，避免預設點擊至下一頁
+        loadImage(arrUrl[currentIndex - 1]);
 
         $(window).on('scroll', function() {
-            var nowVal = $(window).scrollTop() + 1500;
-            var isNeedLoadNext = nowVal > $('div#cp_img').height();
+            var nowVal = $(window).scrollTop() + 2000;
+            var imgVal = $('img#' + versionTag + currentIndex).height();
+            var isNeedLoadNext = nowVal > imgVal;
 
             if (isNeedLoadNext &&
                 currentIndex < arrUrl.length &&
                 !isLock) {
-                loadImage(arrUrl[currentIndex]);
-                currentIndex++;
+                loadImage(arrUrl[currentIndex - 1]);
             }
         });
 
-
         function loadImage(apiUrl) {
             isLock = true;
-            $.ajax({
-                url: apiUrl,
-                type: "GET",
-                success: function(result) {
-                    var url = eval(result)[0];
-                    var html = '<br/><img src="' + url + '"><br/>';
-                    $('img#cp_image').remove();
-                    $('div#cp_img').append(html);
-                    isLock = false;
-                }
+            $.get(apiUrl, function(result) {
+                $('img#cp_image, p#imgloading').remove();
+
+                var tmpImgUrl = eval(result)[0];
+                var html = '<br/><img src="' + tmpImgUrl + '" id="' + versionTag + currentIndex + '" alt="' + authorTag + '"><br/>';
+
+                $('div#cp_img').append(html);
+                currentIndex++;
+
+                isLock = false;
             });
         }
 
-        //remove tool
+        //remove ggyy dom
         $('.topTool, .rightToolBar, .viewTool').remove();
     }
+
+    (function() {
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-100906309-1', 'auto');
+        ga('send', 'pageview');
+    })();
 })();
--- src/Wnacg.js
+++ src/Wnacg.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         exFastComic - Wnacg
-// @namespace    http://blog.exfast.me/
-// @version      0.1
+// @namespace    https://github.com/shuangrain/exFastComic
+// @version      v20170612
 // @description  滾滾滾看漫畫 !!
-// @author       shuangrain07@gmail.com
+// @author       shuangrain
 // @match        http*://www.wnacg.com/photos-view-*
 // @match        http*://www.wnacg.com/?ctl=photos&act=view&id=*
 // @grant        none
@@ -12,50 +12,82 @@
 (function() {
     'use strict';
 
-    //hide ad
-    $('div.couplet').remove();
-    var pageUrl = $('select.pageselect').attr('onchange').replace("location='", "").replace("'+this.value", "");
-    var pageNum = $('select.pageselect').find('option');
+    var versionTag = 'exFastComic_v20170612_';
+    var authorTag = 'https://github.com/shuangrain/exFastComic';
 
-    var currentIndex = 0;
-    var currentId = $('form#tzform').find('input[name="pid"]').val();
-    var arrUrl = [];
-    var arrImage = [];
+    var currentIndex = function() {
+        var tmp = $('.pageselect').find('option:selected').text();
+        tmp = tmp.substring(1, tmp.length - 1);
 
-    //get all url
-    $.each(pageNum, function(index, item) {
-        var value = $(item).val();
-        arrUrl.push(pageUrl + value);
+        return parseInt(tmp);
+    }();
+    var imgApi = $('select.pageselect').attr('onchange').replace("location='", "").replace("'+this.value", "");
+    var isLock = false;
 
-        if (value == currentId) {
-            currentIndex = index;
-        }
-    });
+    var arrUrl = function() {
+        var tmpArr = [];
+        var pageDom = $('select.pageselect').find('option');
 
-    //get image url
-    $.each(arrUrl, function(index, item) {
-        $.get(item, function(result) {
-            arrImage.push({
-                Index: index,
-                Item: $(result).find('img#picarea')[0]
-            });
-
-            if (index == arrUrl.length - 1) {
-                arrImage.sort(function(a, b) {
-                    return parseInt(a.Index) - parseInt(b.Index);
-                });
-            }
+        $.each(pageDom, function(index, item) {
+            var value = $(item).val();
+            tmpArr.push(imgApi + value);
         });
-    });
-    $(window).on('scroll', function() {
-        var allVal = $(document).height() - $(window).height();
-        var nowVal = $(window).scrollTop();
-        var footVal = $('.newpagewrap').height() + $('.tocaowrap').height() + $('.bodywrap').height() + $('.footer').height();
-        var isNeedLoadNext = (allVal - nowVal) < (footVal + 50);
 
-        if (isNeedLoadNext && (currentIndex + 1) < arrImage.length) {
-            $('span#imgarea').append(arrImage[currentIndex + 1].Item);
-            currentIndex++;
+        return tmpArr;
+    }();
+
+    //處理前兩張
+    (function() {
+        var tmpImgUrl = $('img#picarea').attr('src');
+        var html = '<br/><img src="' + tmpImgUrl + '" id="' + versionTag + currentIndex + '" alt="' + authorTag + '"><br/>';
+        $('span#imgarea').html('').append(html);
+        if (currentIndex < arrUrl.length) {
+            loadImage(arrUrl[currentIndex]);
+        }
+    })();
+
+    $(window).on('scroll', function() {
+        var nowVal = $(window).scrollTop() + 2000;
+        var imgVal = $('img#' + versionTag + currentIndex).height();
+        var isNeedLoadNext = nowVal > imgVal;
+
+        if (isNeedLoadNext &&
+            currentIndex < arrUrl.length &&
+            !isLock) {
+            loadImage(arrUrl[currentIndex]);
         }
     });
+
+    function loadImage(apiUrl) {
+        isLock = true;
+        $.get(apiUrl, function(result) {
+            var tmpImgUrl = $($(result).find('img#picarea')[0]).attr('src');
+            var html = '<br/><img src="' + tmpImgUrl + '" id="' + versionTag + (currentIndex + 1) + '" alt="' + authorTag + '"><br/>';
+
+            $('span#imgarea').append(html);
+
+            currentIndex++;
+            isLock = false;
+        });
+    }
+
+    //remove ggyy dom
+    $('div.couplet, div#header, div#bread, div#bodywrap, div.tocaowrap, div.newpagewrap').remove();
+
+    (function() {
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-100906309-1', 'auto');
+        ga('send', 'pageview');
+    })();
 })();
